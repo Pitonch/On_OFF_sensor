039734b35e49ddf6f42d04245ab067af32fa191b
v.romanyuk <v.romanyuk@borosp.by>
2022-10-03 16:42:18 +0300

    add new commit project start

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from interaction import views as interaction_views
+
 
 # POST
 # /interaction/
@@ -8,7 +9,9 @@ urlpatterns = [
     path('about/', interaction_views.about, name='url_to_about'),
     path('settings/', interaction_views.settings, name='url_to_settings'),
     path('commands/', interaction_views.commands, name='url_to_commands'),
-    path('commands/off_sensor/', interaction_views.off_sensor, name='off_sensor'),
-    path('commands/on_sensor/', interaction_views.on_sensor, name='on_sensor'),
+    # re_path(r'^commands/sensor/On|off', interaction_views.sensor, name='sensor'),
+    # path('commands/off_sensor/', interaction_views.off_sensor, name='off_sensor'),
+    # path('commands/on_sensor/', interaction_views.on_sensor, name='on_sensor'),
+    path('commands/<status>', interaction_views.sensor, name='sensor'),
     path('commands/next/', interaction_views.next_pages, name='next'),
     ]
