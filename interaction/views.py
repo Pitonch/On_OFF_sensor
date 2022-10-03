3e78a4d7646361719516eec67771c1a26799c9f0
v.romanyuk <v.romanyuk@borosp.by>
2022-10-03 15:23:53 +0300

    solved problem  button html

--- interaction/views.py
+++ interaction/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 
-
 def home(request):
     # print(request.user)
     return render(request, 'interaction/base.html')
@@ -27,36 +26,36 @@ def next_pages(request):
 
 # отправляем запрос GET на датчик (IP получаем через регистрации по WIFI).
 #правильный способ!!!!.
-# def sensor(request, status: str):
-#     url_sensor = 'http://192.168.0.89/'
-#     switch = requests.post(url_sensor + f'cm?cmnd=Power%20{status}')
-#     print(switch)
-#     switch.raise_for_status()
-#     result = switch.json()
-#     print(result)
-#     return JsonResponse(result)
+def sensor(request, status: str):
+    url_sensor = 'http://192.168.0.89/'
+    switch = requests.post(url_sensor + f'cm?cmnd=Power%20{status}')
+    print(switch)
+    switch.raise_for_status()
+    result = switch.json()
+    print(result)
+    return JsonResponse(result)
 
 # костыль, пока не решу вопрос с html
 
-def on_sensor(res_on):
-    url_sensor1 = 'http://192.168.0.89/'
-    on = 'cm?cmnd=Power%20On'
-    res_on = requests.get(url_sensor1+on)
-    print(res_on)
-    res_on.raise_for_status()
-    result_dict = res_on.json()
-    print(result_dict)
-    return JsonResponse(result_dict)
-
-
-def off_sensor(res_off):
-    url_sensor1 = 'http://192.168.0.89/'
-    off = 'cm?cmnd=Power%20off'
-    res_off = requests.get(url_sensor1 + off)
-    # res_off.raise_for_status()
-    result_dict = res_off.json()
-    print(result_dict)
-    return JsonResponse(result_dict)
+# def on_sensor(res_on):
+#     url_sensor1 = 'http://192.168.0.89/'
+#     on = 'cm?cmnd=Power%20On'
+#     res_on = requests.get(url_sensor1+on)
+#     print(res_on)
+#     res_on.raise_for_status()
+#     result_dict = res_on.json()
+#     print(result_dict)
+#     return JsonResponse(result_dict)
+#
+#
+# def off_sensor(res_off):
+#     url_sensor1 = 'http://192.168.0.89/'
+#     off = 'cm?cmnd=Power%20off'
+#     res_off = requests.get(url_sensor1 + off)
+#     # res_off.raise_for_status()
+#     result_dict = res_off.json()
+#     print(result_dict)
+#     return JsonResponse(result_dict)
 
 
 
