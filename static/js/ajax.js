b0ab7f6ed2bf4cab8d76b47d47fb2dee749195ac
user <v.romanyuk@borosp.by>
2022-10-12 23:33:00 +0300

    add db status (on/off)

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
 })
 
 function sensor_switch(event) {
@@ -15,3 +15,51 @@ function sensor_switch(event) {
 
     $('#click_ON').click(sensor_switch);
     $('#click_OFF').click(sensor_switch);
+
+
+
+
+
+
+// function sensor_switch(status) {
+//     $.ajax({
+//         url: 'commands/sensor/' + status,
+//         type: 'GET',
+//         success: function( data ) {
+//           // Здесь можно создать проверку на вкл/откл датчика
+//         }
+//     });
+// }
+
+// function  sensor_switch(status) {
+//     let request_data = {
+// csrfmiddlewaretoken: csrfmiddlewaretoken, $('input[name=csrfmiddlewaretoken]')[0].value
+//     }
+//
+//     $.ajax({
+//         url: 'commands/sensor/' + status,
+//         type: 'POST',
+//         data: request_data,
+//         success: function( data ) {
+//           // Здесь можно создать проверку на вкл/откл датчика
+//         }
+//     });
+// }
+
+// $(document).ready(function () {
+//
+// })
+//
+// function sensor_switch(event) {
+//     event.preventDefault();
+//         $.ajax({
+//             method : 'GET',
+//             url: $(this).attr('ip_sensor'),
+//             data: "",
+//             dataType: 'json',
+//             success: (function( data ) {})
+//     })
+// }
+//
+//     $('#click_ON').click(sensor_switch);
+//     $('#click_OFF').click(sensor_switch);
