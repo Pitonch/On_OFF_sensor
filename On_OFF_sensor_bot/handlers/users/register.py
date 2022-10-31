b2e0cdaae0d64c22f9e6e92fe9f76e9375d85c33
v.romanyuk <v.romanyuk@borosp.by>
2022-10-31 14:47:30 +0300

    add IsPrivate() filter custom

--- On_OFF_sensor_bot/handlers/users/register.py
+++ On_OFF_sensor_bot/handlers/users/register.py
@@ -2,14 +2,27 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
+from On_OFF_sensor_bot.filters import IsPrivate
+from On_OFF_sensor_bot.keyboards.default import kb_menu
 from On_OFF_sensor_bot.loader import dp
 
 from On_OFF_sensor_bot.states import Register
 
 
-@dp.message_handler(Command('register'))  # /register будет работать вот это
+@dp.message_handler(IsPrivate(), Command('register'))  # /register будет работать вот это
 async def register(message: types.Message):
-    await message.answer('Привет, ты начал регистрацию, \nвведи свое имя')
+    from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+    name = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text=f'{message.from_user.full_name}')
+            ],
+            ],
+        resize_keyboard=True
+    )
+
+    await message.answer('Привет, ты начал регистрацию, \nвведи свое имя', reply_markup=name)
     await Register.test1.set()  # ввод имени
 
 
@@ -23,7 +36,7 @@ async def state1(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler(state=Register.test2)  # ловим сообщение по состоянию register.test1
-async def state1(message: types.Message, state: FSMContext):
+async def state2(message: types.Message, state: FSMContext):
     answer = message.text
 
     await state.update_data(test2=answer)
@@ -32,7 +45,7 @@ async def state1(message: types.Message, state: FSMContext):
     years = data.get('test2')
     await message.answer(f'регистрация завершена\n'
                          f'твое имя {name}\n'
-                         f'тебе {years} лет')
+                         f'тебе {years} лет', reply_markup=kb_menu)
 
     await state.finish()
 
--- On_OFF_sensor_bot/handlers/users/start.py
+++ On_OFF_sensor_bot/handlers/users/start.py
@@ -1,8 +1,9 @@
 from aiogram import types
 from On_OFF_sensor_bot.loader import dp
+from On_OFF_sensor_bot.filters import IsPrivate
 
 
-@dp.message_handler(text='/start')
+@dp.message_handler(IsPrivate(), text='/start')
 async def command_start(message: types.Message):
     await message.answer(f'Привет {message.from_user.full_name}!\n'
                          f'Твой ID: {message.from_user.id}')
