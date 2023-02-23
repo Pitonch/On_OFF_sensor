20963d05f1b651381d4253ef13e712280b3c6a72
user <v.romanyuk@borosp.by>
2023-02-23 23:32:48 +0300

    DB on server OK)

--- On_OFF_sensor/settings.py
+++ On_OFF_sensor/settings.py
@@ -87,7 +87,8 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        #'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'NAME': 'On_OFF_sensor_db',
         'NAME': 'on_off_sensor_db',
         #'USER': 'On_OFF_sensor',
@@ -135,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
